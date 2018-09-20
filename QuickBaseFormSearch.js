3bf4af3fe04ef0a4c97e299e85627d372a6a5a15
Justin Torrence <justin.torrence@logicalposition.com>
2018-09-20 15:42:44 -0700

    Formatting

--- QuickBaseFormSearch.js
+++ QuickBaseFormSearch.js
@@ -10,73 +10,69 @@
 // @updateURL    https://github.com/jtorrence0/QuickBaseTamperMonkey/raw/master/QuickBaseFormSearch.js
 // ==/UserScript==
 
-(function () {
-    'use strict';
+let lastSearch = "";
 
-    let lastSearch = "";
+function searchForm() {
+    let searchString = document.getElementById("customSearchInput").value.toLowerCase();
 
-    function searchForm() {
-        let searchString = document.getElementById("customSearchInput").value.toLowerCase();
+    let matches = [];
 
-        let matches = [];
-
-        for (let i = 0; i < selElems.length; i++) {
-            let optionText = selElems[i].options[selElems[i].selectedIndex].text.toLowerCase();
-            if(optionText.includes(searchString))
-                matches.push(i)
-        }
-
-        let currentIndex = matches.indexOf(GetFirstSelectedRow());
-        let nextIndex = currentIndex += 1;
-
-        if (lastSearch == searchString && matches.length > currentIndex)
-            showTheElement(matches[nextIndex]);
-        else
-            showTheElement(matches[0]);
-
-        lastSearch = searchString;
+    for (let i = 0; i < selElems.length; i++) {
+        let optionText = selElems[i].options[selElems[i].selectedIndex].text.toLowerCase();
+        if (optionText.includes(searchString))
+            matches.push(i)
     }
 
-    function searchUI() {
-        if (!document.getElementById("customSearchGroup")) {
-            let searchGroup = document.createElement("div");
-            searchGroup.id = "customSearchGroup";
+    let currentIndex = matches.indexOf(GetFirstSelectedRow());
+    let nextIndex = currentIndex += 1;
 
-            // input
-            let input = document.createElement("input");
-            input.classList.add("UserInput");
-            input.classList.add("WithPadding");
-            input.placeholder = "Search Form Elements";
-            input.id = "customSearchInput";
+    if (lastSearch == searchString && matches.length > currentIndex)
+        showTheElement(matches[nextIndex]);
+    else
+        showTheElement(matches[0]);
 
-            // search button
-            let confirmSearch = document.createElement("button");
-            confirmSearch.classList.add("Vibrant");
-            confirmSearch.classList.add("Primary");
-            confirmSearch.classList.add("Secondary");
-            confirmSearch.onclick = searchForm;
-            confirmSearch.innerText = "Search";
+    lastSearch = searchString;
+}
 
-            searchGroup.appendChild(input);
-            searchGroup.appendChild(confirmSearch);
+function searchUI() {
+    if (!document.getElementById("customSearchGroup")) {
+        let searchGroup = document.createElement("div");
+        searchGroup.id = "customSearchGroup";
 
-            let introText = document.getElementById("introText")
-            introText.appendChild(searchGroup);
-        }
+        // input
+        let input = document.createElement("input");
+        input.classList.add("UserInput");
+        input.classList.add("WithPadding");
+        input.placeholder = "Search Form Elements";
+        input.id = "customSearchInput";
 
+        // search button
+        let confirmSearch = document.createElement("button");
+        confirmSearch.classList.add("Vibrant");
+        confirmSearch.classList.add("Primary");
+        confirmSearch.classList.add("Secondary");
+        confirmSearch.onclick = searchForm;
+        confirmSearch.innerText = "Search";
+
+        searchGroup.appendChild(input);
+        searchGroup.appendChild(confirmSearch);
+
+        let introText = document.getElementById("introText")
+        introText.appendChild(searchGroup);
     }
 
-    function showTheElement(index) {
-        selElems[index].click();
-        selElems[index].scrollIntoView();
-    }
+}
+
+function showTheElement(index) {
+    selElems[index].click();
+    selElems[index].scrollIntoView();
+}
 
 
-    let searchButton = document.createElement("button");
-    searchButton.classList.add("Vibrant");
-    searchButton.onclick = searchUI;
-    searchButton.innerText = "Search Form";
+let searchButton = document.createElement("button");
+searchButton.classList.add("Vibrant");
+searchButton.onclick = searchUI;
+searchButton.innerText = "Search Form";
 
-    let nav = document.getElementById("pageNavBarActions");
-    nav.appendChild(searchButton);
-})();
+let nav = document.getElementById("pageNavBarActions");
+nav.appendChild(searchButton);
