f0de543868f77906ed92c9b0db03139a9f555ba1
Justin Torrence <justin.torrence@logicalposition.com>
2018-10-24 16:07:06 -0700

    Added support for table home screens

--- QuickBaseCSVExp.user.js
+++ QuickBaseCSVExp.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         QuickBase CSV Export
 // @namespace    QuickBaseTamperMonkey
-// @version      0.6
+// @version      1.0
 // @description  Exports Quick Base reports to CSV
 // @author       Justin Torrence
 // @match        ://*.quickbase.com/db/*?a=q*
+// @match        ://*.quickbase.com/db/*?a=td*
 // @grant        none
 // @updateURL    https://github.com/jtorrence0/QuickBaseTamperMonkey/raw/master/QuickBaseCSVExp.user.js
 // ==/UserScript==
